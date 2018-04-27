3d76b9f9a433b25a032fceaa047da8304b7aee21
Unknown <icabezasgomez@gmail.com>
2018-04-27 20:29:20 +0200

    [ADD] Created DAOs

---
+++ src/java/daos/RasgosDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Esta clase contiene las funciones de gestion de Categoria, Raza, Tipo, Caracteristica
+ *
+ */
+package daos;
+
+/**
+ *
+ * @author THOR
+ */
+public class RasgosDAO {
+    
+}
